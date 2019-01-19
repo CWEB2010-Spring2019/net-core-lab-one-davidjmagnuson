008c4b6c06c7f616f1fa9eccd14569827690a030
davidjmagnuson <46498677+davidjmagnuson@users.noreply.github.com>
2019-01-18 18:11:17 -0600

    added questions and answers

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -11,81 +11,86 @@ namespace lab_one
         static void Main()
         {
             //declaration statements
-            string correctAnswers[10] = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
-            string userSelection[10];
-            string quizQuestions[10] = {"question",
-                                        "question",
-                                        "question",
-                                        "question",
-                                        "question",
-                                        "question",
-                                        "question",
-                                        "question",
-                                        "question",
-                                        "question",};
-            string quizQuestionOptions[,] =
+            string[] correctAnswers = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
+            string[] userSelection;
+            string[] quizQuestions = {"When was .NET core released?",
+                                        "Who developed .NET Core?",
+                                        "What is the current version of .NET core?",
+                                        "What language is .NET core written in?",
+                                        "What operating system is .NET core for?",
+                                        "What kind of framework is .NET core?",
+                                        ".NET core is a type of what?",
+                                        "When was the current version of .NET core released?",
+                                        "The host for a .NET core web application is configured in what kind of file?",
+                                        "Every command in .NET core command line starts with...?",};
+            string[ , ] quizQuestionOptions =
                                     {
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
-                                        {"A) <answer>", "B) <answer>", "C) <answer>", "D) <answer>"},
+                                        {"A) 2005", "B) 2016", "C) 2012", "D) 2015"},
+                                        {"A) Intel", "B) Microsoft", "C) Apple", "D) Google"},
+                                        {"A) 1.3", "B) 2.1", "C) 2.2", "D) 5.0"},
+                                        {"A) C#", "B) Python", "C) Swift", "D) Java"},
+                                        {"A) Windows", "B) Linux", "C) MacOS", "D) All the above"},
+                                        {"A) open-sourced", "B) lisenced", "C) irrelevant", "D) GUI"},
+                                        {"A) software", "B) hard drive", "C) CPU", "D) RAM"},
+                                        {"A) 2011", "B) 2016", "C) 2018", "D) 2019"},
+                                        {"A) program.py", "B) startup.cs", "C) middleware", "D) program.cs"},
+                                        {"A) dotnet", "B) .net", "C) dot", "D) dotcore"},
                                     };
             string selection;
-            num start
-            num EXIT = -1; //sentinel value
+            double start;
+            double EXIT = -1; //sentinel value
 
             List<string> correct = new List<string>();
             List<string> wrong = new List<string>();
 
             Console.WriteLine("Welcome, this program will assess your knowledge of .NET Core");
             Console.WriteLine("To get started, please enter a number other than -1 to start the program");
-            INPUT start();
-            
-            while start != EXIT
+            start = Console.ReadKey();
 
-                    for(num x = 0; x < quizQuestions.Length; x++)
+            while (start != EXIT)
+            {
+
+                for (double x = 0; x < quizQuestions.Length; x++)
+                {
+                    Console.WriteLine(quizQuestions[x]);
+                    for (num i = 0; i < quizQuestionOptions.GetLength(1); i++)
                     {
-                        Console.WriteLine(quizQuestions[x]);
-                        for(num i = 0; i < quizQuestionOptions.GetLength(1); i++)
-                        {
-                            Console.WriteLine(quizQuestionOptions[x, i]);
-                        }
-                        Console.WriteLine("Please enter an Option");
-                        INPUT selection 
-                        userSelection[x] = selection
-
-                        //check if the useer is correct
-                        if(selection == correctAnswers[x])then
-                            correct.Add(selection)
-                        else
-                            wrong.Add("Question: " + (x + 1) + ") " + selection)
-                        endif
+                        Console.WriteLine(quizQuestionOptions[x, i]);
                     }
+                    Console.WriteLine("Please enter an Option");
+                    selection = Console.ReadKey();
+                    userSelection[x] = selection;
 
+                    //check if the user is correct
+                    if (selection == correctAnswers[x])
+                    {
+                        correct.Add(selection);
+                    }
+                    else
+                    {
+                        wrong.Add("Question: " + (x + 1) + ") " + selection);
+                    }
+                }
                 Console.WriteLine("You answered " + correct.Count + " out of " + quizQuestions.Length);
 
                 //determine if they passed
                 var results = (correct.Count >= 7) ? "You passed the assessment " : " You didn't pass ";
 
                 //output the wrong questions
-                if(wrong.Count > 0)then
+                if (wrong.Count > 0)
+                {
                     Console.WriteLine("Listed below are the questions you got wrong");
-                    wrong.ForEach(x => { Console.WriteLine(x)});
-                endif 
+                    wrong.ForEach(x => { Console.WriteLine(x);});
+                }
 
-                wrong.Clear()
-                correct.Clear()
+                wrong.Clear();
+                correct.Clear();
+                userSelection.Clear();
 
                 Console.WriteLine("To take the quiz again, enter any value other than -1. To exit enter -1");
-                input start
+                start = Console.ReadKey();
 
-            endwhile
+            }
 
 
             Console.WriteLine("Program has concluded");
