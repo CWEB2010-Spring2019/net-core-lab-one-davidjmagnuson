3916f9a136e46eaf8835690e731fe8826e830252
david magnuson <magdavj@dunwoody.edu>
2019-01-28 17:00:47 -0600

    finalcode

--- lab_one/Program.cs
+++ lab_one/Program.cs
@@ -6,24 +6,24 @@ using System.Collections.Generic;
 
 namespace lab_one
 {
-    class Program
+    internal class Program
     {
-        static void Main()
+        private static void Main()
         {
             //declaration statements
             string[] correctAnswers = { "B", "B", "C", "A", "D", "A", "A", "C", "D", "A" };
-            string[] userSelection;
-            string[] quizQuestions = {"When was .NET core released?",
-                                        "Who developed .NET Core?",
-                                        "What is the current version of .NET core?",
-                                        "What language is .NET core written in?",
-                                        "What operating system is .NET core for?",
-                                        "What kind of framework is .NET core?",
-                                        ".NET core is a type of what?",
-                                        "When was the current version of .NET core released?",
-                                        "The host for a .NET core web application is configured in what kind of file?",
-                                        "Every command in .NET core command line starts with...?",};
-            string[ , ] quizQuestionOptions =
+            string[] userSelection = new string[10];
+            string[] quizQuestions = {"1. When was .NET core released?",
+                                        "2. Who developed .NET Core?",
+                                        "3. What is the current version of .NET core?",
+                                        "4. What language is .NET core written in?",
+                                        "5. What operating system is .NET core for?",
+                                        "6. What kind of framework is .NET core?",
+                                        "7. .NET core is a type of what?",
+                                        "8. When was the current version of .NET core released?",
+                                        "9. The host for a .NET core web application is configured in what kind of file?",
+                                        "10. Every command in .NET core command line starts with...?",};
+            string[,] quizQuestionOptions =
                                     {
                                         {"A) 2005", "B) 2016", "C) 2012", "D) 2015"},
                                         {"A) Intel", "B) Microsoft", "C) Apple", "D) Google"},
@@ -34,31 +34,31 @@ namespace lab_one
                                         {"A) software", "B) hard drive", "C) CPU", "D) RAM"},
                                         {"A) 2011", "B) 2016", "C) 2018", "D) 2019"},
                                         {"A) program.py", "B) startup.cs", "C) middleware", "D) program.cs"},
-                                        {"A) dotnet", "B) .net", "C) dot", "D) dotcore"},
+                                        {"A) dotnet", "B) .net", "C) dot", "D) dotcore"}
                                     };
             string selection;
-            double start;
-            double EXIT = -1; //sentinel value
+            string start;
+            string EXIT = "x"; //sentinel value
 
             List<string> correct = new List<string>();
             List<string> wrong = new List<string>();
 
             Console.WriteLine("Welcome, this program will assess your knowledge of .NET Core");
-            Console.WriteLine("To get started, please enter a number other than -1 to start the program");
-            start = Console.ReadKey();
+            Console.WriteLine("To get started, please press the Enter key");
+            start = Console.ReadLine();
 
             while (start != EXIT)
             {
-
-                for (double x = 0; x < quizQuestions.Length; x++)
+                Console.Clear();
+                for (int x = 0; x < quizQuestions.Length; x++)
                 {
                     Console.WriteLine(quizQuestions[x]);
-                    for (num i = 0; i < quizQuestionOptions.GetLength(1); i++)
+                    for (int i = 0; i < quizQuestionOptions.GetLength(1); i++)
                     {
                         Console.WriteLine(quizQuestionOptions[x, i]);
                     }
                     Console.WriteLine("Please enter an Option");
-                    selection = Console.ReadKey();
+                    selection = Console.ReadLine();
                     userSelection[x] = selection;
 
                     //check if the user is correct
@@ -70,6 +70,7 @@ namespace lab_one
                     {
                         wrong.Add("Question: " + (x + 1) + ") " + selection);
                     }
+                    Console.Clear();
                 }
                 Console.WriteLine("You answered " + correct.Count + " out of " + quizQuestions.Length);
 
@@ -80,19 +81,18 @@ namespace lab_one
                 if (wrong.Count > 0)
                 {
                     Console.WriteLine("Listed below are the questions you got wrong");
-                    wrong.ForEach(x => { Console.WriteLine(x);});
+                    wrong.ForEach(x => { Console.WriteLine(x); });
                 }
 
                 wrong.Clear();
                 correct.Clear();
-                userSelection.Clear();
 
-                Console.WriteLine("To take the quiz again, enter any value other than -1. To exit enter -1");
-                start = Console.ReadKey();
+                Console.WriteLine("To take the quiz again, please press any key except 'X'. To exit enter 'X'");
+                start = Console.ReadLine();
 
             }
-
             Console.WriteLine("Program has concluded");
+
         }
     }
 }
